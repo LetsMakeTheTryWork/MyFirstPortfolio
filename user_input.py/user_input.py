1aef5416c3675f2f922cb79ecbb529fc9158a00d
LetsMakeTheTryWork <rocckyrocky001@gmail.com>
2024-05-27 20:20:53 +0300

    Add files via upload

---
+++ user_input.py/user_input.py
@@ -0,0 +1,10 @@
+
+
+name= input("Enter user name:");
+
+age= input("Enter your age:")
+
+location= input("Enter your location:")
+
+
+printf("Hello {name}, you are{age} years old and live in {location}")
